0ddffbc1f512f5b9eda86699eefa9bd456abe257
53845714nF <seba98feustel@gmail.com>
2023-04-22 15:36:07 +0200

    Add Certform to the Button in the Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,18 +1,28 @@
 import { ReactElement, useState } from "react";
 import useLogout from "../hooks/useLogout";
 import CreateCaForm from "./forms/CreateCaForm";
+import CreateCertForm from "./forms/CreateCertForm";
 
 
 export default function Header(): ReactElement {
     const logout = useLogout();
     const [isOpenCaForm, setCaForm] = useState(false);
+    const [isOpenCertForm, setCertForm] = useState(false);
     
-    function openCaForm(): void {
-        setCaForm(true);
+    function openCaForm(): void{
+        setCaForm(true)
     }
 
     function closeCaForm(): void{
-        setCaForm(false);
+        setCaForm(false)
+    }
+
+    function openCertForm(): void{
+        setCertForm(true)
+    }
+
+    function closeCertForm(): void{
+        setCertForm(false)
     }
     
     return (
@@ -23,7 +33,8 @@ export default function Header(): ReactElement {
                 </ul>
                 <ul>
                     <li>
-                        <button type="submit" className="outline">Create Cert</button>
+                        <button type="submit" className="outline" onClick={openCertForm}>Create Cert</button>
+                        <CreateCertForm isOpen={isOpenCertForm} closeForm={closeCertForm}/>
                     </li>
                     <li>
                         <button type="submit" className="contrast outline" onClick={openCaForm}>Create CA</button>
